34b3456307ba517c61e2fc23f5cfbfe807d030f5
Christophe Savard <christophe_savard@hotmail.ca>
2018-03-20 02:12:12 -0400

    Initial push

---
+++ Pi-Approximation/Pi-Approximation/MainForm.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace Pi_Approximation
+{
+    public partial class MainForm : Form
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes the form
+        /// </summary>
+        /// <inheritdoc/>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
